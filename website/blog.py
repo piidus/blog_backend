96d35bad6bbf9f89b70b5add74de1ccc3f56788b
Sudiip <115084777+piidus@users.noreply.github.com>
2024-10-27 16:08:12 +0530

    check language

--- website/blog.py
+++ website/blog.py
@@ -12,17 +12,21 @@ blog = Blueprint('blog', __name__)
 
 @blog.route('/dashboard')
 def dashboard():
-    # get all pincode from database
-    all_pincode = Pincode.query.filter(Pincode.pincode == 700135).all()
-    # print(all_pincode)
-    data = []
-    data = {'pincode':all_pincode}
-
+   
 
    
     current_app.logger.info("info message")
-    # current_app.logger.warning("warning message")
-    # current_app.logger.error("error message")
-
+    data = []
     return render_template('blog/dashboard.html', data=data)
+
+
+@blog.route('/blog-page/<string:blog_id>', methods=['GET', 'POST'])
+def blog_page(**kwargs):  
+    blog_id = kwargs['blog_id']  
+    if blog_id is None:
+        all_pincode = Pincode.query.filter(Pincode.pincode == 700135).all()
+    # print(all_pincode)
+    
+    data = {'pincode':all_pincode}
+    return render_template('blog/blog-page.html', data=data)
 
