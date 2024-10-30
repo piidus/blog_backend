284abcc6ade1bfc7d521716390e9241a78ea2c65
Sudiip <115084777+piidus@users.noreply.github.com>
2024-10-30 12:10:00 +0530

    modal added

--- website/auth.py
+++ website/auth.py
@@ -1,6 +1,7 @@
 from flask_login import current_user, login_user, logout_user
-from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask import Blueprint, render_template, request, flash, redirect, url_for, jsonify
 from .models import db,  User
+import random
 
 
 auth = Blueprint('auth', __name__)
@@ -9,23 +10,43 @@ auth = Blueprint('auth', __name__)
 @auth.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
-        email = request.form.get('email')
-        first_name = request.form.get('firstName')
-        last_name = request.form.get('lastName')
-        phone = request.form.get('phone')
-        password = request.form.get('password')
-        user = User.query.filter_by(email=email).first()
-        if user:
-            flash('User already exists.', 'danger')
-        else:
-            new_user = User(email=email, first_name=first_name, last_name=last_name, phone=phone, password=password)
-            db.session.add(new_user)
-            db.session.commit()
-            login_user(new_user)
-            return redirect(url_for('auth.login'))
+        print(request.json)
+        email = request.json['email']
+        first_name = request.json['firstName']
+        last_name = request.json['lastName']
+        phone = request.json['phone']
+        password = request.json['password']
+        print(email, first_name, last_name, phone, password) 
+        # check valus are not empty
+        if email == '' or first_name == '' or last_name == '' or phone == '' or password == '':
+            return jsonify({'success': False, 'message': 'All fields are required!'}), 400
+        
+    # if request.method == 'POST':
+    #     email = request.form.get('email')
+    #     first_name = request.form.get('firstName')
+    #     last_name = request.form.get('lastName')
+    #     phone = request.form.get('phone')
+    #     password = request.form.get('password')
+    #     user = User.query.filter_by(email=email).first()
+    #     if user:
+    #         flash('User already exists.', 'danger')
+    #     else:
+    #         new_user = User(email=email, first_name=first_name, last_name=last_name, phone=phone, password=password)
+    #         db.session.add(new_user)
+    #         db.session.commit()
+    #         login_user(new_user)
+    #         return redirect(url_for('auth.login'))
     return render_template('auth/signup.html', user=current_user)
 
+# return a six digit code
+@auth.route('/auth/authcode', methods=['POST'])
+def authcode():
+    email = request.form.get('email')
+    # user = User.query.filter_by(email=email).first()
+    authcode = random.randint(100000, 999999)
+    return jsonify ({'authcode': authcode})
 
+# login route
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
---
+++ website/static/auth/signup.js
@@ -0,0 +1,70 @@
+var code;
+function signup() {
+    // fetch a code from backend and set it in the modal as hidden input
+    fetch("/auth/authcode", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ 'email': $("#email").val() }),
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            code = data['authcode'];
+            // console.log("Success:", data);
+            console.log(code);
+        })
+        .catch((error) => {
+            console.error("Error:", error);
+        });
+    // activate modal
+    $("#staticBackdrop").modal("show");
+}
+
+function checkCode() {
+    // get input
+    var input = document.getElementById("code").value;
+    // check if code is correct
+    if (code == input) {
+        // if correct, close modal
+        $("#staticBackdrop").modal("hide");
+        // create post request to backend
+        var signupForm = document.getElementById("signupFormid");
+        console.log(signupForm);    
+
+        // signupForm.addEventListener("submit", function (event) {
+        //     event.preventDefault();
+
+            // Get form data
+            const formData = new FormData(signupForm);
+            // console.log(formData);
+            // Convert FormData to a regular object
+            const formObject = Object.fromEntries(formData.entries());
+                // console.log(formObject);
+            // Send form data to backend as JSON
+            fetch("/signup", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(formObject), // Convert the object to JSON string
+            })
+                .then(response => response.json())
+                .then(data => {
+                    console.log("Success:", data);
+                    // if success = false alert
+                    if (data['success'] == false) {
+                        alert(data['message']);
+                    }
+                    // Open the next modal or handle success as needed
+                })
+                .catch((error) => {
+                    console.error("Error:", error);
+                });
+        // });
+
+
+        // console.log(signupForm);
+
+    }
+}
